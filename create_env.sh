0c392ac85a7f2828159318e60eed486669af5c49
Cristiano Köhler <c.koehler@fz-juelich.de>
2024-08-15 13:34:37 +0200

    Check version to avoid deprecated conda parameter

--- create_env.sh
+++ create_env.sh
@@ -1,1 +1,20 @@
-conda env create --force -f ./environment/environment.yaml
+#!/bin/bash
+
+# Newer conda versions use --yes to force environment creation
+CONDA_VERSION=$(conda --version)
+VERSION_MATCH="^conda ([0-9]+)\..+$"
+
+if [[ $CONDA_VERSION =~ $VERSION_MATCH ]]
+then
+   version="${BASH_REMATCH[1]}"
+   if [ ${version} -lt 23 ]
+   then
+      command="--force"
+   else
+      command="--yes"
+   fi
+else
+   command="--yes"
+fi
+
+conda env create "${command}" -f ./environment/environment.yaml
